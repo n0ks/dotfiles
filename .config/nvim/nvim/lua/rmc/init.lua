d926eea9c0e5d7f383af44ebfddb8bd1e557aac3
Rodrigo Martins <rodrigo.m.de.campos@accenture.com>
2021-08-16 18:33:34 -0300

    feat: remove unused folder

--- .config/nvim/nvim/lua/rmc/init.lua
+++
@@ -1,9 +0,0 @@
-require('rmc.telescope')
-require('rmc.flutter-tools')
-require('rmc.lualine')
-require('rmc.projectionist')
-require('rmc.gitsigns')
-require('rmc.treesitter')
--- require('rmc.lsp')
-
-
