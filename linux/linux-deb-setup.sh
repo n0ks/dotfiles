97f77ad6dbd452c6b16562af33567b098a61d7a6
noks <noks@pop-os.localdomain>
2021-10-21 21:56:40 -0300

    feat: script tweaks

--- linux/linux-deb-setup.sh
+++ linux/linux-deb-setup.sh
@@ -7,11 +7,11 @@ echo "-------------------------------------------------";
 echo "";
 
 installSoftware() {
-  echo "[INFO] Installing required software..";
+	echo "[INFO] Installing required software..";
 
-  sudo apt-get install -y zsh git-core curl wget python-pip build-essential \ 
-  autoconf unzip libssl-dev libncurses5-dev libreadline-dev zlib1g-dev \
-  libsqlite3-dev inotify-tools pkg-config fd bat rg fzf tmux git stow screenkey \
+	sudo apt-get install -y zsh curl wget build-essential \
+		autoconf unzip libssl-dev libncurses5-dev libreadline-dev zlib1g-dev \
+		libsqlite3-dev inotify-tools pkg-config fd-find bat ripgrep fzf tmux git stow screenkey \
 
   # Change the shell to zsh
 	echo "[INFO] Changing the shell of this user to use zsh...";
@@ -20,45 +20,43 @@ installSoftware() {
 }
 
 cloneRepos() {
-  echo "mkdir -p ${HOME}/code"
-  mkdir -p "${HOME}/code"
-
-  echo "[INFO] cloning repos"
-  git clone https://github.com/alacritty/alacritty.git "${HOME}/code"
-  git clone https://github.com/neovim/neovim.git "${HOME}/code"
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+	echo "[INFO] cloning repos"
+	git clone https://github.com/n0ks/dotfiles ~/.dotfiles
+	git clone https://github.com/alacritty/alacritty.git ~/code
+	git clone https://github.com/neovim/neovim.git ~/code
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 
 }
 
 dotfilesStow (){
- pushd ~/.dotfiles; chmod +x install.sh; source ./install.sh; popd
- echo `pwd`
+	pushd ~/.dotfiles; chmod +x install.sh; source ./install.sh; popd
+	echo `pwd`
+	source ~/.zshrc
 }
 
 asdfSetup() {
-  source ~/.zshrc
 
 	# Install useful plugins 
 	echo "[INFO] Installing asdf plugins...";
 	source $HOME/.asdf/asdf.sh;
 
-  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git;
-  asdf plugin-add java https://github.com/halcyon/asdf-java.git
-  asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git;
-  asdf plugin-add python
+	asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git;
+	asdf plugin-add java https://github.com/halcyon/asdf-java.git
+	asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git;
+	asdf plugin-add python
 }
 
 setup(){
-    installSoftware;
-    cloneRepos;
-    dotfilesStow;
-    installAsdf;
+	installSoftware;
+	cloneRepos;
+	asdfSetup;
+	dotfilesStow;
 }
 
 read -p "Setup is about to start. Do you want to continue? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		setup;
-	fi;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	setup;
+fi;
 
 
