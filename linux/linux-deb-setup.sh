67f9ec785ff68e0d37b6c61b2b7e2747a8b01e9c
n0ks <rodrigocmp29@gmail.com>
2021-11-02 21:40:06 -0300

    feat: asdf/lua setup

--- linux/linux-deb-setup.sh
+++ linux/linux-deb-setup.sh
@@ -6,57 +6,59 @@ echo "Shell installation script for n0ks dotfiles";
 echo "-------------------------------------------------";
 
 installSoftware() {
-	echo "[INFO] Installing required software..";
+  echo "[INFO] Installing required software..";
 
- sudo apt install -y --ignore-missing ninja-build gettext libtool libtool-bin zsh curl wget python3-pip build-essential cmake g++ \
-  autoconf unzip libssl-dev libncurses5-dev libreadline-dev zlib1g-dev automake doxygen exa xclip \
-  libsqlite3-dev inotify-tools pkg-config fd-find bat ripgrep fzf tmux git stow screenkey
+  sudo apt install -y --ignore-missing ninja-build gettext libtool libtool-bin zsh curl wget python3-pip build-essential cmake g++ \
+    autoconf unzip libssl-dev libncurses5-dev libreadline-dev zlib1g-dev automake doxygen exa xclip \
+    libsqlite3-dev inotify-tools pkg-config fd-find bat ripgrep fzf tmux git stow screenkey
 
-	if ! command -v gh &> /dev/null
-	then
-	  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-	  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-	  sudo apt update
-	  sudo apt install gh
-	fi
+  if ! command -v gh &> /dev/null
+  then
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt update
+    sudo apt install gh
+  fi
 
   # Change the shell to zsh
-	echo "[INFO] Changing the shell of this user to use zsh...";
-	chsh -s $(which zsh)
+  echo "[INFO] Changing the shell of this user to use zsh...";
+  chsh -s $(which zsh)
 
 }
 
 cloneRepos() {
-	echo "[INFO] cloning repos"
-	git clone https://github.com/n0ks/dotfiles ~/.dotfiles
-	git clone https://github.com/alacritty/alacritty.git ~/code
-	git clone https://github.com/neovim/neovim.git ~/code
-	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+  echo "[INFO] cloning repos"
+  git clone https://github.com/n0ks/dotfiles ~/.dotfiles
+  git clone https://github.com/neovim/neovim.git ~/code
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+  git clone https://github.com/sumneko/lua-language-server ~/code
 
 }
 
 dotfilesStow (){
-	pushd ~/.dotfiles; chmod +x install.sh; source ./install.sh; popd
-	echo `pwd`
-	source ~/.zshrc
+  pushd ~/.dotfiles; chmod +x install.sh; source ./install.sh; popd
+  echo `pwd`
+  source ~/.zshrc
 }
 
 asdfSetup() {
 
-	# Install useful plugins 
-	echo "[INFO] Installing asdf plugins...";
-	source $HOME/.asdf/asdf.sh;
+  # Install useful plugins 
+  echo "[INFO] Installing asdf plugins...";
+  source $HOME/.asdf/asdf.sh;
 
-	asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git;
-	asdf plugin-add java https://github.com/halcyon/asdf-java.git
-	asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git;
-	asdf plugin-add python
+  # asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git;
+  # asdf plugin-add java https://github.com/halcyon/asdf-java.git
+  # asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git;
+  # asdf plugin-add python
+
+  # install from .tools-version - test
+  asdf install
 }
 
 neovimSetup(){
   echo "[INFO] setting up vim-plug"
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  npm i -g vscode-langservers-extracted
 
   pushd ~/code/neovim
   rm -rf build/
@@ -65,6 +67,19 @@ neovimSetup(){
   make CMAKE_BUILD_TYPE=Release
   sudo make install
   popd
+
+  pushd ~/code/lua-language-server
+  git submodule update --init --recursive
+  cd 3rd/luamake
+  ./compile/install.sh
+  cd ../../
+  ./3rd/luamake/luamake rebuild
+  popd
+
+}
+
+installNpmPackages(){
+  npm i -g vscode-langservers-extracted
 }
 
 alacrittySetup(){
@@ -72,7 +87,7 @@ alacrittySetup(){
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 #   pushd ~/code/alacritty
-  
+
 #   cargo build --release
 #   infocmp alacritty
 
@@ -80,7 +95,7 @@ alacrittySetup(){
 #   sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 #   sudo desktop-file-install extra/linux/Alacritty.desktop
 #   sudo update-desktop-database
-  # set as default term
+# set as default term
 #   sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 50
 #   sudo update-alternatives --config x-terminal-emulator
 
@@ -91,18 +106,18 @@ alacrittySetup(){
 
 
 setup(){
-	installSoftware;
-	cloneRepos;
-	asdfSetup;
-	dotfilesStow;
-    neovimSetup;
-    alacrittySetup;
+  installSoftware;
+  cloneRepos;
+  dotfilesStow;
+  asdfSetup;
+  neovimSetup;
+  alacrittySetup;
 }
 
 read -p "Setup is about to start. Do you want to continue? (y/n) " -n 1;
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	setup;
+  setup;
 fi;
 
 
