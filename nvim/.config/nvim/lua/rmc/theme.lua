3c2461bc9b56edfa7ee55caf909567cad9bafe12
n0ks <rodrigocmp29@gmail.com>
2021-11-21 22:10:26 -0300

    feat: tweaks

--- nvim/.config/nvim/lua/rmc/theme.lua
+++ nvim/.config/nvim/lua/rmc/theme.lua
@@ -1,54 +1,13 @@
 
-local catppuccino = require("catppuccino")
-
-catppuccino.setup(
-    {
-    colorscheme = "soft_manilo",
-    transparency = false,
-    term_colors = true,
-		styles = {
-			comments = "italic",
-			functions = "italic",
-			keywords = "italic",
-			strings = "NONE",
-			variables = "NONE",
-		},
-		integrations = {
-			treesitter = true,
-			native_lsp = {
-				enabled = true,
-				styles = {
-					errors = "italic",
-					hints = "italic",
-					warnings = "italic",
-					information = "italic"
-				}
-			},
-			lsp_trouble = false,
-			lsp_saga = false,
-			gitgutter = false,
-			gitsigns = true,
-			telescope = true,
-			nvimtree = {
-				enabled = false,
-				show_root = false,
-			},
-			which_key = false,
-			indent_blankline = false,
-			dashboard = false,
-			neogit = false,
-			vim_sneak = false,
-			fern = true,
-			barbar = false,
-			bufferline = false,
-			markdown = false,
-		}
-	}
-)
-
--- catppuccino.load()
 
 vim.g.rose_pine_variant = "moon"
 vim.g.rose_pine_bold_vertical_split_line = true
 vim.cmd("colorscheme rose-pine")
+
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+]])
+
+vim.g.rose_pine_disable_background = true
 
