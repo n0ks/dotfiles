003335f4112a656797f5bacdc09c5f17d12cddef
n0ks <rodrigocmp29@gmail.com>
2022-02-13 22:04:43 -0300

    feat: stow file

--- stow.sh
+++ stow.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -o allexport
-source .env
+source ./.env
 set +o allexport
 
 for folder in $STOW_FOLDERS; do
-	printf "Deleting $STOW_FOLDERS"
-	stow -D $folder
+	echo "Stowing $folder"
+	stow -R "$folder"
 done
