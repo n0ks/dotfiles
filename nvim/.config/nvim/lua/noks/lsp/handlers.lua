46ab2c7df3efdf9caefd86537c6e5e869a62a99b
n0ks <rodrigocmp29@gmail.com>
2022-04-06 20:43:43 -0300

    textDocument/definition direct jump
    thx to tj cash money  💰

--- nvim/.config/nvim/lua/noks/lsp/handlers.lua
+++ nvim/.config/nvim/lua/noks/lsp/handlers.lua
@@ -35,9 +35,24 @@ end
 vim.diagnostic.config(config)
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 M.capabilities = coq.lsp_ensure_capabilities(capabilities)
+
+-- Jump directly to the first available definition every time.
+vim.lsp.handlers["textDocument/definition"] = function(_, result)
+  if not result or vim.tbl_isempty(result) then
+    print "[LSP] Could not find definition"
+    return
+  end
+
+  if vim.tbl_islist(result) then
+    vim.lsp.util.jump_to_location(result[1], "utf-8")
+  else
+    vim.lsp.util.jump_to_location(result, "utf-8")
+  end
+end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" or client.name == "sumneko_lua" or client.name == "html" then
