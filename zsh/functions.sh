51855269ef4882b6409fcf59b69d1e93391afb7a
n0ks <rodrigocmp29@gmail.com>
2022-04-13 21:06:42 -0300

    tweak fzf-tmux split height

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -73,15 +73,17 @@ gwrf() { git worktree remove --force "$*"; }
 fbra() {
   local branches branch
   branches=$(git branch --all | grep -v HEAD) &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+
+  branch=$(echo "$branches" | fzf-tmux -d $(( 10 + $(wc -l <<< "$branches") )) +m) &&
+
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
 
 fbr() {
   local branches branch
   branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+
+  branch=$(echo "$branches" | fzf-tmux -d $(( 10 + $(wc -l <<< "$branches") )) +m) &&
+
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
