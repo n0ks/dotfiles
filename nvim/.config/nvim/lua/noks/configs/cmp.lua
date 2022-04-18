86470f366e360940088c05c6c4cff71801fe7bd0
n0ks <rodrigocmp29@gmail.com>
2022-04-18 20:46:38 -0300

    cmp + luasnips

--- nvim/.config/nvim/lua/noks/configs/cmp.lua
+++ nvim/.config/nvim/lua/noks/configs/cmp.lua
@@ -1,4 +1,13 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
 	snippet = {
@@ -10,18 +19,58 @@ cmp.setup({
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
+	experimental = {
+		ghost_text = true,
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-y"] = cmp.mapping.complete(),
+
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 	}),
 	sources = cmp.config.sources({
-		{ name = "luasnip" }, -- For luasnip users.
-	}, {
-		{ name = "buffer" },
+		{ name = "luasnip" },
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "path" },
+		{ name = "buffer", keyword_length = 4 },
 	}),
+	formatting = {
+		format = require("lspkind").cmp_format({
+			with_text = true,
+			menu = {
+				buffer = "[Buffer]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[LSP]",
+				luasnip = "[LuaSnip]",
+				path = "[Path]",
+			},
+		}),
+	},
 })
 
 -- Set configuration for specific filetype.
@@ -50,3 +99,5 @@ cmp.setup.cmdline(":", {
 		{ name = "cmdline" },
 	}),
 })
+
+
---
+++ nvim/.config/nvim/lua/noks/configs/luasnip.lua
@@ -0,0 +1,5 @@
+-- require("luasnip.loaders.from_vscode").lazy_load()
+--
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
+
+require("luasnip").filetype_extend("dart", { "flutter" })
