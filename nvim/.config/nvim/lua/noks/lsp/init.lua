4480bea278b5ce46a667163d5bfbbd2a682979d3
n0ks <rodrigocmp29@gmail.com>
2022-07-07 20:17:24 -0300

    refactor: lsp installer setup

--- nvim/.config/nvim/lua/noks/lsp/init.lua
+++ nvim/.config/nvim/lua/noks/lsp/init.lua
@@ -2,9 +2,34 @@ local sumneko_lua_server = require("noks.lsp.servers.sumneko")
 local tsserver = require("noks.lsp.servers.tsserver")
 local null_ls = require("noks.lsp.servers.null_ls")
 local flutter = require("noks.lsp.servers.flutter")
+local lsp_install = require("nvim-lsp-installer")
+local lsp_config = require("lspconfig")
+local on_attach = require("noks.lsp.handlers").on_attach
+local capabilities = require("noks.lsp.handlers").capabilities
 
-sumneko_lua_server.setup()
-tsserver.setup()
-null_ls.setup()
-flutter.setup()
-require("go").setup()
+local pyright_settings = require("noks.lsp.servers.pyright")
+local jsonls_settings = require("noks.lsp.servers.jsonls")
+
+local M = {}
+
+M.setup = function()
+	sumneko_lua_server.setup()
+	tsserver.setup()
+	null_ls.setup()
+	flutter.setup()
+
+	local server_args = { jsonls = jsonls_settings, pyright = pyright_settings }
+
+	local servers = lsp_install.get_installed_servers()
+
+	for _, server in ipairs(servers) do
+		local args = server_args[server.name] or {}
+
+		args.on_attach = on_attach
+		args.capabilities = capabilities
+
+		lsp_config[server.name].setup(args)
+	end
+end
+
+return M
--- nvim/.config/nvim/lua/noks/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/noks/lsp/lsp-installer.lua
@@ -1,24 +1,14 @@
-local lsp_installer = require("nvim-lsp-installer")
-local capabilities = require("noks.lsp.handlers").capabilities
-local on_attach = require("noks.lsp.handlers").on_attach
-local utils = require("noks.configs.utils")
-local pyright_settings = require("noks.lsp.servers.pyright")
-local jsonls_settings = require("noks.lsp.servers.jsonls")
-local inspect = require("noks.configs.inspect")
-
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		capabilities = capabilities,
-		on_attach = on_attach,
-	}
-
-	if server.name == "pyright" then
-		utils.merge(opts, pyright_settings)
-	end
-
-	if server.name == "jsonls" then
-		utils.merge(opts, jsonls_settings)
-	end
-
-	server:setup(opts)
-end)
+require("nvim-lsp-installer").setup({
+	ensure_installed = {
+		"bashls",
+		"cssls",
+		"emmet_ls",
+		"yamlls",
+		"vimls",
+		"sqlls",
+		"marksman",
+		"jsonls",
+		"html",
+	},
+	automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+})
