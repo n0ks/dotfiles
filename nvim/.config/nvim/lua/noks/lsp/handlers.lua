3bef9f09a2fe384237d7cb4ce1a8bae9cfb5d51a
n0ks <rodrigocmp29@gmail.com>
2022-10-06 18:22:07 -0300

    auto format on save

--- nvim/.config/nvim/lua/noks/lsp/handlers.lua
+++ nvim/.config/nvim/lua/noks/lsp/handlers.lua
@@ -60,10 +60,32 @@ vim.lsp.handlers["textDocument/definition"] = function(_, result)
 	end
 end
 
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 M.on_attach = function(client, bufnr)
-	if client.name == "tsserver" or client.name == "sumneko_lua" or client.name == "html" then
-		client.server_capabilities.document_formatting = false
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
 	end
+
+	-- if client.name == "tsserver" or client.name == "sumneko_lua" or client.name == "html" then
+	-- 	client.server_capabilities.document_formatting = false
+	-- end
 
 	navic.setup({
 		highlight = true,
