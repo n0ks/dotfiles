e913e589c597344eddff47e59de9242a3b31a20d
n0ks <rodrigocmp29@gmail.com>
2022-12-07 16:17:52 -0300

    diffview cfg

--- nvim/.config/nvim/lua/noks/configs/projectionist.lua
+++ nvim/.config/nvim/lua/noks/configs/projectionist.lua
@@ -1,27 +1,27 @@
--- vim.g.projectionist_heuristics = {
--- 	["lib/*.dart"] = {
--- 		["lib/src/*.dart"] = {
--- 			alternate = "test/{}_test.dart",
--- 			type = "source",
--- 		},
--- 		["test/*_test.dart"] = {
--- 			alternate = { "lib/src/{}.dart" },
--- 			type = "test",
--- 			template = {
--- 				"import 'package:flutter_test/flutter_test.dart';",
--- 				"import 'package:mocktail/mocktail.dart';",
--- 				"",
--- 				"void main() async {",
--- 				"  group('{basename|camelcase|capitalize}', () {",
--- 				"    // TODO:",
--- 				"    test('', (){open}{close})",
--- 				"  });",
--- 				"}",
--- 			},
--- 		},
--- 		["lib/util/*.dart"] = {
--- 			alternate = "test/{}_test.dart",
--- 			type = "source",
--- 		},
--- 	},
--- }
+vim.g.projectionist_heuristics = {
+	["lib/*.dart"] = {
+		["lib/src/*.dart"] = {
+			alternate = "test/{}_test.dart",
+			type = "source",
+		},
+		["test/*_test.dart"] = {
+			alternate = { "lib/src/{}.dart" },
+			type = "test",
+			template = {
+				"import 'package:flutter_test/flutter_test.dart';",
+				"import 'package:mocktail/mocktail.dart';",
+				"",
+				"void main() async {",
+				"  group('{basename|camelcase|capitalize}', () {",
+				"    // TODO:",
+				"    test('', (){open}{close})",
+				"  });",
+				"}",
+			},
+		},
+		["lib/util/*.dart"] = {
+			alternate = "test/{}_test.dart",
+			type = "source",
+		},
+	},
+}
