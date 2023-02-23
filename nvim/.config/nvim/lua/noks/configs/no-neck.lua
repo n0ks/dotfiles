d149b6fa3fb842649a347c466d31a5896d2484a7
n0ks <rodrigocmp29@gmail.com>
2023-02-23 10:25:40 -0300

    add no-neck cfg

---
+++ nvim/.config/nvim/lua/noks/configs/no-neck.lua
@@ -0,0 +1,13 @@
+require("no-neck-pain").setup({
+	buffers = {
+		blend = 0.2,
+		scratchPad = {
+			enabled = true,
+			filename = "notes",
+			-- location = "~/Documents/",
+		},
+		bo = {
+			filetype = "md",
+		},
+	},
+})
