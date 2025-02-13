31f7738172552f61c81fdc49c20a348699ff0881
n0ks <22434056+n0ks@users.noreply.github.com>
2025-02-13 09:48:57 -0300

    add mini

---
+++ nvim/.config/nvim/lua/noks/plugins/mini.lua
@@ -0,0 +1,8 @@
+return {
+
+  { "echasnovski/mini.pairs", version = false, config = true },
+  { "echasnovski/mini.ai", version = false, config = true },
+  { "echasnovski/mini.surround", version = false, config = true },
+  { "echasnovski/mini.statusline", version = false, config = true },
+
+}
